71f40fe1006aaeecef43f2e2e7d8aa68c2d4eb4e
Craig Jolley <jolleycraig@gmail.com>
2016-09-08 15:54:50 -0400

    Add "nowcasting" of 2016 data based on GSMA data.

--- nowcast-mobile.R
+++ nowcast-mobile.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 library(llamar)
+library(XLConnect)
+source('read-DHS.R')
+
 
 ###############################################################################
 # To what extent does the "sigmoidal" wealth/mobile relationship hold
@@ -13,7 +16,6 @@ wealth_mobile <- function(df,text) {
     ggtitle(text) +
     theme_classic() 
 }
-
 
 wealth_mobile(rwa_2015,'Rwanda 2015')
 
@@ -62,10 +64,121 @@ rbind(uga_2006,uga_2011) %>% removeAttributes %>%
 rbind(zmb_2007,zmb_2014) %>% removeAttributes %>%
   mobile_compare(title='Zambia 2007-2014')
 
-# TODO: How do DHS penetration rates compare to those other sources? Can I use them 
-# to estimate what a comparable DHS penetration value would be for a certain country and year?
-# Linear regression might do it; something from caret might be better.
-# The different measurements don't exactly compare apples to apples.
+###############################################################################
+# Test agreement between my weighted averages and numbers from DHS API
+###############################################################################
+# % of households with mobile phone
+test <- loadDHS(indicators='HC_HEFF_H_MPH',countries='RW',years=2015)
+test$Value
+w_mean(rwa_2015$mobile,rwa_2015$weight)
+# Glad to see that works.
+
+###############################################################################
+# Get as many mobile penetration averages as I can from DHS. Focus on SS
+# Africa.
+###############################################################################
+africa <- c('BJ','BF','BU','CM','CV','CF','TD','CG','CD','CI','EK','ER',
+            'ET','GA','GM','GH','GN','KE','LS','LB','MD','MW','ML','MZ','NM',
+            'NI','NG','RW','SN','SL','ZA','SD','SZ','TZ','TG','UG','ZM','ZW')
+
+afr_mobile <- loadDHS(indicators='HC_HEFF_H_MPH',
+                      countries=paste(africa,collapse=',')) %>%
+  select(CountryName,SurveyYear,Value)
+# 72 data points. 
+write.csv(afr_mobile,'DHS_mobile.csv',row.names=FALSE)
+
+###############################################################################
+# Get the corresponding data points out of the (overly-pretty) GSMA Excel 
+# reports.
+###############################################################################
+p2frac <- function(p) {
+  # convert a percentage string to a number
+  p %>% sub('%','',.) %>% as.numeric / 100
+}
+
+s2num <- function(s) {
+  # get those commas out of there
+  s %>% gsub(',','',.) %>% as.numeric
+}
+
+get_gsma <- function(fname,yrs) {
+  x <- read.csv(fname,skip=2,stringsAsFactors=FALSE)
+  qnames <- paste('Q4.',yrs,sep='')
+  market <- x[x$Metric=='Unique subscribers' &
+                x$Attribute=='Total','Market..Operator']
+  agu <- x[x$Metric=='Growth rate, unique subscribers, annual' &
+             x$Attribute=='Total',qnames] %>% t %>% p2frac
+  pu <- x[x$Metric=='Market penetration, unique subscribers' &
+            x$Attribute=='Total',qnames] %>% t %>% p2frac
+  agn <- x[x$Metric=='Growth rate, excluding cellular M2M, annual' &
+             x$Attribute=='Total' & x$Market..Operator==market,qnames] %>% t %>% p2frac
+  p <- x[x$Metric=='Market penetration' & x$Attribute=='Total' & 
+           x$Market..Operator==market,qnames] %>% t %>% p2frac
+  h <- x[x$Metric=='Herfindahl-Hirschman Index',qnames] %>% t %>% s2num
+  res <- cbind(agu,pu,agn,p,h) %>% as.data.frame
+  res$CountryName <- market
+  res$SurveyYear <- yrs
+  names(res) <- c('ann_growth_uniq','penetration_uniq',
+                  'ann_growth','penetration','herfindahl',
+                  'CountryName','SurveyYear')
+  res
+}
+dirroot <- 'C:/Users/Craig/Desktop/Live projects/Pay-go solar/hh survey data/GSMA/'
+
+gsma <- data.frame()
+for (n in afr_mobile$CountryName %>% unique) {
+  #print(n)
+  fname <- paste0(dirroot,n,'.csv')
+  yrs <- afr_mobile[afr_mobile$CountryName==n,'SurveyYear']
+  gsma <- rbind(gsma,get_gsma(fname,yrs))
+}
+gsma[gsma$CountryName=="Congo, Democratic Republic", 'CountryName'] <- "Congo Democratic Republic"
+gsma[gsma$CountryName=="C?te d'Ivoire", 'CountryName'] <- "Cote d'Ivoire"
+
+###############################################################################
+# How well can the subscriber-level GSMA data predict the household-level
+# DHS data? 
+###############################################################################
+
+j <- plyr::join(afr_mobile,gsma,by=c('CountryName','SurveyYear')) %>% unique
+
+# First, try linear models, omitting country information
+lm(j$Value ~ j$penetration_uniq) %>% summary               # R^2 = 0.7914
+lm(j$Value ~ j$penetration) %>% summary                    # R^2 = 0.7672
+j2 <- j %>% select(-CountryName)
+lm(Value ~ .,j2) %>% summary # R^2 = 0.8329
+
+# I tried to beat this with caret and didn't really succeed. 
+
+# Include country as factor variable
+lm(Value ~ CountryName,j) %>% summary            # R^2 = 0.601
+lm(Value ~ CountryName+SurveyYear,j) %>% summary # R^2 = 0.948
+lm(Value ~ .,j) %>% summary                      # R^2 = 0.977
+# In addition to the country factor variable, the annual growth rate
+# and penetration of unique subscribers seem to be important. The year stops
+# being significant when these are factored in.
+
+###############################################################################
+# Project 2016 household-level rates
+###############################################################################
+pa <- c('Ethiopia','Ghana','Kenya','Liberia','Malawi','Nigeria','Rwanda',
+        'Senegal','Sierra Leone','Tanzania','Uganda','Zambia')
+gsma_2016 <- data.frame()
+for (x in pa) {
+  fname <- paste0(dirroot,x,'.csv')
+  gsma_2016 <- rbind(gsma_2016,get_gsma(fname,2016))
+}
+fit <- lm(Value ~ .,j)
+newvals <- predict(fit,gsma_2016)
+wpred <- gsma_2016 %>% 
+  dplyr::select(CountryName,SurveyYear) %>% 
+  mutate(Value=newvals,
+         Value=ifelse(Value>100,100,Value)) %>%
+  rbind(afr_mobile[afr_mobile$CountryName %in% pa,])
+ggplot(wpred,aes(x=SurveyYear,y=Value,group=CountryName,color=CountryName)) +
+  geom_point(size=4) +
+  geom_line(size=2) + 
+  theme_classic()
 
 # TODO: Some of the mobile adoption numbers are old -- my hypothesis is that I could 
 # get more up-to-date numbers by assuming that new mobile subscribers are added in order
@@ -73,5 +186,5 @@ rbind(zmb_2007,zmb_2014) %>% removeAttributes %>%
 # and adding in new numbers until the total reaches values similar to what we see from
 # GSMA, WB, etc. GSMA probably better due to higher time resolution.
 
-# TODO: Can I get comparable estimates (i.e. weighted averages) using the DHS API from
-# other countries/years?
+# Need a function to add columns to my existing datasets with estimates of who
+# gained cell phones by 2016.
