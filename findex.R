279bc3348bced118f2b60834c5a47780d403a814
Craig Jolley <jolleycraig@gmail.com>
2016-09-20 21:53:29 -0400

    More findex analysis

--- findex.R
+++ findex.R
@@ -39,8 +39,14 @@ head(nmis_series) %>% plyr::join(key,by='series_code')
 tail(nmis_series) %>% plyr::join(key,by='series_code')
 
 # No good way to get around missing data; let's impute.
-system.time(gf_mice <- mice(gf_wide,m=1)) # will probably take overnight
-# do manually with medians; not great but will let us do PCA
+system.time(gf_mice <- mice(gf_wide,m=1,method='fastpmm')) 
+# running this for 9.87h got me 631 variable imputations ~ 64/hr
+# 477 varibles x 5 iterations = 2385 imputations = 37.3h
+# doing this properly will have to wait until the weekend;
+# it seems that changing algorithms doesn't help much when the dataset
+# is wide rather than long; they're all slow
+
+# do manually with medians; not good but will let us do PCA
 gf_imp <- gf_wide[,2:ncol(gf_wide)]
 for (n in names(gf_imp)) {
   isna <- is.na(gf_imp[,n])
@@ -57,7 +63,7 @@ qplot(pr$x[,1],pr$x[,2],color=1) +
 # What are our PCs capturing?
 gf_wide$country_name[pr$x[,1] > 25] # rich countries
 gf_wide$country_name[pr$x[,1] < -17] # poor countries
-gf_wide$country_name[pr$x[,2] > 10] # E&E, MENA, LAC
+gf_wide$country_name[pr$x[,2] > 14] # E&E, MENA, LAC
 gf_wide$country_name[pr$x[,2] < -30] # Africa
 
 
@@ -77,20 +83,137 @@ pr$rotation[,2] %>% sort %>% tail %>% code_trans
 # Not seeing as clear of a pattern here
 hist(pr$rotation[,2],breaks=30)
 
-# k-means clustering to find groups
+###############################################################################
+# Use inflection in between-cluster variance to choose appropriate number of
+# clusters.
+###############################################################################
+res <- data.frame()
 cluster_us <- pr$x %>% as.data.frame %>%
   select(PC1:PC26) 
-km <- kmeans(cluster_us,centers=5) # totally arbitrary for now
+for (i in 2:30) {
+  f <- NULL
+  for (j in 1:20) {
+    km_j <- kmeans(cluster_us,centers=i)
+    f <- c(f,km_j$betweenss/km_j$totss)
+  }
+  res <- rbind(res,data.frame(i=i,f=mean(f)))
+}
+qplot(res$i,res$f) + 
+  xlab('Number of clusters') +
+  ylab('Fraction of between-cluster variance') +
+  geom_vline(xintercept=6,color='red') +
+  theme_classic() # I'd say it's around k=6
+
+###############################################################################
+# Split into clusters and visualize on first two PCs.
+###############################################################################
+km <- kmeans(cluster_us,centers=6) # totally arbitrary for now
 cluster_us$clust <- as.factor(km$cluster)
 cluster_us$country_name <- gf_wide$country_name
-cluster_us[cluster_us$clust==5,'country_name']
 
 ggplot(cluster_us,aes(x=PC1,y=PC2,group=clust,color=clust)) +
   geom_point(size=3) +
   theme_classic()
 
-# TODO: More rigorous choice of cluster size (variance 'elbow' plot)
+cluster_us[cluster_us$clust==4,'country_name']
 
-# TODO: Find a "typical" country in each cluster, based on its centroid
+###############################################################################
+# Find a "typical" country in each cluster, based on its centroid
+###############################################################################
+country_centroid <- function(clust_num,num_res=1) {
+  mid <- km$centers[clust_num,]
+  tmp <- cluster_us[cluster_us$clust==clust_num,]
+  tmp$d2 <- sapply(1:nrow(tmp), function(i) sum((mid - tmp[i,1:26])^2))
+  tmp %>% arrange(d2) %>% head(num_res)
+}
 
-# TODO: Find variables that distinguish clusters from each other (log regression)
+country_centroid(6,5) %>% select(country_name,d2)
+
+###############################################################################
+# Find variables that distinguish clusters from each other 
+###############################################################################
+sep_vars <- function(gp1,gp2) {
+  # gp1 and gp2 are each data frames
+  tmp <- rbind(gp1 %>% mutate(label=1),gp2 %>% mutate(label=0))
+  # remove variables with zero variance
+  keep_us <- apply(tmp,2,sd) > 0
+  tmp <- tmp[,keep_us]
+  pvals <- 
+  
+  plyr::ldply(names(tmp %>% select(-label)),function(x) {
+    tt <- t.test(tmp[tmp$label==1,x],tmp[tmp$label==0,x])
+    data.frame(series_code=x,m1=tt$estimate[1],m2=tt$estimate[2],pval=tt$p.value)
+  }) %>%
+    #filter(pval < 0.01/ncol(tmp)) %>%
+    arrange(pval) %>%
+    plyr::join(key,by='series_code')
+}
+
+sep_vars(gf_imp[cluster_us$clust==5,],gf_imp[cluster_us$clust==4,]) %>%
+  head(10)
+
+# Primary difference between cluster 1 and cluster 6: More people in cluster 1 
+# use the internet for paying bills, shopping, etc.
+
+# Clusters 6 and 2: People in 6 more likely to pay bills using accounts
+# at financial institutions, and to have accounts
+
+# Clusters 2 and 3: People in 2 more likely to have debit cards
+
+# Clusters 3 and 5: People in 5 use more domestic remittances; people in 3 
+# pay more utility bills
+
+# Clusters 5 and 4: People in 4 more likely to have mobile accounts and use 
+# them for domestic remittances and utility bills
+
+sep_vars(gf_imp[cluster_us$PC1 > 0,],gf_imp[cluster_us$PC1 < 0,]) %>% head(10)
+sep_vars(gf_imp[cluster_us$PC1 > 10,],gf_imp[cluster_us$PC1 < -10,]) %>% head(10)
+# Major PC1 separation is whether people have accounts at financial institutions
+
+sep_vars(gf_imp[cluster_us$PC2 > 0,],gf_imp[cluster_us$PC2 < 0,]) %>% head(10)
+sep_vars(gf_imp[cluster_us$PC2 > 10,],gf_imp[cluster_us$PC2 < -10,]) %>% head(10)
+# PC2 separation: people in countries with PC2 < 0 save and borrow more than others
+
+###############################################################################
+# Focus on Power Africa countries (add to interest list as needed)
+###############################################################################
+
+pa <- c('Ethiopia','Ghana','Kenya','Malawi','Nigeria','Rwanda',
+        'Senegal','Sierra Leone','South Africa','Tanzania','Uganda','Zambia')
+
+# Hardly any information is available for Liberia (only 2 of 477 variables), so
+# I'm not including it.
+
+# Keep the 445 variables that were measured for all countries
+gf_pa <- gf_wide[gf_wide$country_name %in% pa,]
+gf_pa <- gf_pa[,colSums(is.na(gf_pa)) == 0]
+
+# Visualize
+pa_plot <- function(code) {
+  tmp <- gf_pa[,c('country_name',code)]
+  names(tmp) <- c('country','value')
+  tmp <- arrange(tmp,value)
+  tmp$country <- factor(tmp$country,levels=tmp$country)
+  tmp$text <- round(tmp$value) %>% paste0('%')
+  title <- key[key$series_code==code,'series_name'] %>% 
+    gsub(' \\[w2\\]','',.)
+  ggplot(tmp,aes(x=country,y=value)) +
+    geom_bar(stat='identity',fill='skyblue') +
+    geom_text(aes(label=text),hjust=-0.1) +
+    coord_flip() +
+    ggtitle(title) +
+    xlab('') + ylab('') +
+    theme_classic() +
+    theme(axis.ticks = element_blank(),
+          axis.text.x = element_blank())
+}
+
+pa_plot('WP14887_7.10') # Account at a financial institution, rural
+pa_plot('WP14918.1')    # Bought from a store on credit
+pa_plot('WP15163_4.1')  # Mobile accts
+pa_plot('WP15163_4.8')  # Mobile accts, poorest 40%
+pa_plot('WP14934.1')    # Received domestic remittances
+pa_plot('WP14928.1')    # Sent domestic remittances
+
+# TODO: Add ability to highlight a country of interest with another color
+# TODO: Charts for mobile ownership, electrification, fuel subsidies with similar layout
