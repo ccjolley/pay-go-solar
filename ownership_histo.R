bc69fae7ac9eb464f1b15292f6c337b067bfc1e0
Craig Jolley <jolleycraig@gmail.com>
2016-11-14 11:58:11 -0500

    ownership_histo.R is ready to go

--- ownership_histo.R
+++ ownership_histo.R
@@ -1,4 +1,5 @@
-library(ggplot)
+library(ggplot2)
+library(ggrepel)
 library(dplyr)
 source('read-findex.R')
 
@@ -11,8 +12,6 @@ tmp <- gf_wide %>% select(country_name,m_acct=WP15163_4.1) %>%
   filter(!grepl('income',country_name)) %>%
   filter(!country_name %in% c('World','South Asia')) %>%
   mutate(m_acct = m_acct/100)
-# top <- tmp %>% filter(m_acct > 10 | country_name == 'Nigeria') %>% arrange(m_acct) %>%
-#   mutate(y = c(1,7:1,4:1,3:1,1) / 200 + 0.01)
 
 top <- tmp %>% filter(m_acct > 0.02) %>% arrange(m_acct) %>%
   mutate(y = c(28:1,8:1,4:1,3:1,1) / 1.5,
