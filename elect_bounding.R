73b8d32d8eb0711499864b559e63aa1f33f8ab4f
Craig Jolley <jolleycraig@gmail.com>
2017-07-11 11:52:04 -0400

    Add feature engineering

--- elect_bounding.R
+++ elect_bounding.R
@@ -52,17 +52,25 @@ ggplot(uga_border) +
 #   Assumes that df will have columns named 'lat','long', and 'elect'
 #   wrapper should be a function that will return an array of predictions
 ###############################################################################
-make_elect_shp <- function(df,wrapper,out_name=NULL,thresh=0.5) {
+make_elect_shp <- function(df,wrapper,out_name=NULL,thresh=0.5,
+                           enrich_pts=NULL) {
   # Raster setup
   xpad <- (max(df$long) - min(df$long))*0.05
   ypad <- (max(df$lat) - min(df$lat))*0.05
   res <- 0.008333333 # same as LandScan
+  print('Making raster r1')
   r1 <- raster(vals=-1,res=res,
                xmn=min(df$long)-xpad,xmx=max(df$long)+xpad,
                ymn=min(df$lat)-ypad,ymx=max(df$lat)+ypad)
   r1pts <- rasterToPoints(r1) 
+  if (!is.null(enrich_pts)) {
+    print('Enriching raster points')
+    r1pts <- enrich_pts(r1pts)
+  }
   # Call wrapper function
+  print('Caling wrapper')
   pred <- wrapper(df,r1pts)
+  print('Finishing up')
   r1 <- setValues(r1,pred)
   if (!is.null(out_name)) {
     r2 <- setValues(r1,pred > 0.5)
@@ -96,7 +104,9 @@ qplot(uga$elect,uga$pred)
 # RMSE = 0.1768631 
 
 knn_wrap <- function(df,pts) {
-  my_knn <- knn.reg(train=df[,c('lat','long')],test=pts[,c('y','x')],
+  vars <- setdiff(names(df),'elect')
+  pts <- pts %>% as.data.frame %>% dplyr::rename(lat=y,long=x)
+  my_knn <- knn.reg(train=df[,vars],test=pts[,vars],
                     y=df$elect,k=7,algorithm='kd_tree')
   my_knn$pred
 }
@@ -223,7 +233,7 @@ rf_wrap <- function(df,pts) {
   x_train <- df %>% dplyr::select(-elect)
   x_pred <- pts %>% as.data.frame %>%
     dplyr::rename(long=x,lat=y) %>%
-    dplyr::select(lat,long)
+    dplyr::select(-layer)
   my_rf <- randomForest(x=x_train,y=df$elect,
                         xtest=x_pred)
   my_rf$test$predicted
@@ -234,77 +244,84 @@ rf_raster <- make_elect_shp(uga,rf_wrap)
 uga_raster_plot(rf_raster)
 
 ###############################################################################
-# The big guns: XGBoost
+# Feature engineering: enrich the original dataset with distances from Uganda's
+# major urban centers (which can be derived from lat/long).
 ###############################################################################
-library(xgboost)
-x <- uga[,c('lat','long')] %>% as.matrix
-train_xgb <- xgboost(data=x,
-                     label=uga$elect,
-                     objective='reg:linear',
-                     nrounds=500,
-                     verbose=0)
-(uga$elect - predict(train_xgb,x))^2 %>% mean %>% sqrt # RSME=0.00238 overfit.
-# Actually, it overfits so much that none of the on-grid points being evaluated
-# for the raster are >0.5 -- it's useless for interpolation because it just
-# memorized the data.
+library(ggmap)
+library(geosphere)
 
-# Cross-validation to the rescue
-xgb_rmse <- function(df,in_test,nrounds,eta,max_depth,subsample,cbt) {
-  df_test <- df[in_test,]
-  df_train <- df[!in_test,]
-  x <- df_train[,c('lat','long')] %>% as.matrix
-  x_test <- df_test[,c('lat','long')] %>% as.matrix
-  df_xgb <- xgboost(data=x,
-                    label=df_train$elect,
-                    objective='reg:linear',
-                    nrounds=nrounds,
-                    verbose=0,
-                    eta=eta,max_depth=max_depth,subsample=subsample,
-                    colsample_bytree=cbt)
-  (df_test$elect - predict(df_xgb,x_test))^2 %>% mean %>% sqrt
+# Get coordinates of landmark cities
+
+cities <- c('Kitgum','Arua','Gulu','Lira','Obalang','Soroti','Mbale','Jinja',
+            'Kampala','Entebbe','Fort Portal','Kasese','Mitoma','Mbarara',
+            'Rushenyi')
+geo_cities <- data.frame(city=cities) %>%
+  mutate(str=paste0(city,', Uganda'))
+geo_cities <- cbind(geo_cities,geocode(geo_cities$str,output='latlon'))
+
+# Add distances to data frame
+
+distFlat <- function(x,y) {
+  sqrt((x[1]-y[1])^2 + (x[2]-y[2])^2)
 }
 
-xgb_xval <- function(df,verbose=FALSE,fold=10,nrounds=385, eta=0.05, max_depth=5, 
-                     subsample=0.7,cbt=0.7) {
-  xval_ind <- rep(1:fold,times=nrow(df)/fold+1)[1:nrow(df)] %>% sample
-  res <- rep(NA,fold)
-  for (i in 1:fold) {
-    rmsle <- xgb_rmse(df,xval_ind==i,nrounds,eta,max_depth,subsample,cbt)
-    if (verbose) {
-      paste0('x-val ',i,': RMSLE = ',rmsle) %>% print
-    }
-    res[i] <- rmsle
+add_dists <- function(df,fun=distFlat) {
+  res <- as.data.frame(df)
+  if ('lat' %in% names(df)) {
+    xy <- c('long','lat')
+  } else {
+    xy <- c('x','y')
   }
-  mean(res)
-}
-
-xgb_xval(uga,FALSE,10,385,0.05,5,0.7,0.7) # 0.1944931
-xgb_xval(uga,FALSE,10,200,0.05,5,0.7,0.7) # 0.1884459
-xgb_xval(uga,FALSE,10,100,0.05,5,0.7,0.7) # 0.1842703
-# Can't seem to improve on this much with small tweaks
-
-
-
-# Now let's make a raster
-xgb_wrap <- function(df,pts) {
-  x <- df[,c('lat','long')] %>% as.matrix
-  my_xgb <- xgboost(data=x,
-                    label=df$elect,
-                    objective='reg:linear',
-                    nrounds=100,
-                    eta=0.05,max_depth=5,
-                    subsample=0.7,colsample_bytree=0.7,
-                    verbose=0)
-  pts_rename <- pts %>% as.data.frame %>%
-    dplyr::rename(long=x,lat=y) %>%
-    dplyr::select(lat,long) %>%
-    as.matrix
-  res <- predict(my_xgb,pts_rename)
-  paste0('max = ',max(res)) %>% print
-  paste0('min = ',min(res)) %>% print
+  for (i in 1:nrow(geo_cities)) {
+    x <- df[,xy] %>% as.matrix
+    y <- geo_cities[i,c('lon','lat')]
+    c <- geo_cities$city[i] %>% as.character
+    paste0('Adding ',c) %>% print
+    #dim(x) %>% print
+    #dim(res) %>% print
+    #d <- distm(x,y,fun=fun) %>% as.numeric
+    #colnames(d) <- c
+    #res <- cbind(res,d)
+    res[,c] <- distm(x,y,fun=fun)
+  }
   res
 }
+uga_geo <- add_dists(uga)
+uga_geo_cos <- add_dists(uga,fun=distCosine)
+qplot(uga_geo$Kitgum,uga_geo_cos$Kitgum) # good enough for me
 
-xgb_raster <- make_elect_shp(uga,xgb_wrap,'uga_xgb')
-plot(xgb_raster)
-# This looks horrible. This algorithm is definitely overkill.
+### Linear regression with new features
+
+lm_raster2 <- make_elect_shp(uga_geo,lm_wrap,enrich_pts=add_dists)
+# This is taking a really long time. Distance measurements are really slow
+uga_raster_plot(lm_raster2)
+
+### KNN with new features
+knn_raster2 <- make_elect_shp(uga_geo,knn_wrap,enrich_pts=add_dists)
+uga_raster_plot(knn_raster2)
+
+### RF with new features
+rf_raster2 <- make_elect_shp(uga_geo,rf_wrap,enrich_pts=add_dists)
+uga_raster_plot(rf_raster2)
+
+### SVM with new features
+svm_raster2 <- make_elect_shp(uga_geo,svm_wrap,enrich_pts=add_dists)
+svm_vals2 <- (svm_raster2 %>% rasterToPoints)[,3]
+svm_filter2 <- ifelse(svm_vals2>1,1,ifelse(svm_vals2<0,0,svm_vals2))
+svm_filter_raster2 <- setValues(svm_raster2,svm_filter2)
+uga_raster_plot(svm_filter_raster2)
+
+###############################################################################
+# Lousy feature engineering
+###############################################################################
+islands <- c('Bugala Island','Kome Island','Sigulu Island','Lolui Island')
+geo_cities <- data.frame(city=islands) %>%
+  mutate(str=paste0(city,', Uganda'))
+geo_cities <- cbind(geo_cities,geocode(geo_cities$str,output='latlon'))
+uga_geo <- add_dists(uga)
+
+svm_raster3 <- make_elect_shp(uga_geo,svm_wrap,enrich_pts=add_dists)
+svm_vals3 <- (svm_raster3 %>% rasterToPoints)[,3]
+svm_filter3 <- ifelse(svm_vals3>1,1,ifelse(svm_vals3<0,0,svm_vals3))
+svm_filter_raster3 <- setValues(svm_raster3,svm_filter3)
+uga_raster_plot(svm_filter_raster3)
