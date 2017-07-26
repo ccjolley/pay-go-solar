c3ae76d7d444333f504a7d9927acbac7e9d01162
Craig Jolley <jolleycraig@gmail.com>
2017-07-26 17:04:44 -0400

    Found a better way to do it.

--- low_density.R
+++ low_density.R
@@ -1,3 +1,89 @@
+### Better way to do this:
+# Randomly sample points from LandScan raster
+# keep only those that are within boundaries and have low pop.
+# continue until reaching a specified number of unique points
+# output dataframe with new points that can be fed into another
+# round (to do this iteratively)
+
+library(raster)
+library(rgdal)
+library(dplyr)
+
+# define ROI
+tzn_data <- read.csv('tzn_dhs_2016.csv') %>% filter(lat != 0)
+xmin <- min(tzn_data$long)
+xmax <- max(tzn_data$long)
+xbuf <- 0.05*(xmax-xmin)
+ymin <- min(tzn_data$lat)
+ymax <- max(tzn_data$lat)
+ybuf <- 0.05*(ymax-ymin)
+# load LandScan raster and clip
+f <- "C:/Users/Craig/Desktop/Live projects/bivar-raster/landscan-pop/w001001x.adf"
+ls <- raster(f) %>%
+  crop(extent(xmin-xbuf,xmax+xbuf,ymin-ybuf,ymax+ybuf))
+
+tzn_data$pop_dens <- extract(ls,tzn_data[,c('long','lat')] %>% as.matrix)
+
+quantile(tzn_data$pop_dens,na.rm=TRUE,probs=c(0,0.05,0.95,1))
+sort(tzn_data$pop_dens) %>% head(50)
+# Quite a few points actually have pop density zero. 
+
+### Given a lat/long pair, find the distance to the closest point in a df
+closest_dist <- function(lat_,long_,df,omit.zero=TRUE) {
+  df_i <- df %>%
+    mutate(d2=(lat-lat_)^2 + (long-long_)^2)
+  if (omit.zero) {
+    df_i <- filter(df_i,d2>0)
+  }
+  min(df_i$d2) %>% sqrt
+}
+
+# TODO: not sure if this is really the optimal way to nest the if/thens
+# need to actually benchmark
+get_zero_pts <- function(r,shp,npts,pt_df,cutoff=1,dist_cutoff=0.5,seed=12345) {
+  set.seed(seed)
+  res <- data.frame()
+  while (nrow(res) < npts) {
+    x <- runif(1,min=r@extent@xmin,max=r@extent@xmax)
+    y <- runif(1,min=r@extent@ymin,max=r@extent@ymax)
+    c <- closest_dist(y,x,pt_df)
+    if (c > dist_cutoff) { # check if too close to sample points
+      p <- extract(r,cbind(x,y))
+      if (!is.na(p) && p < cutoff) { # check for zero population density
+        pt <- data.frame(x=x,y=y)
+        coordinates(pt) <- ~x+y
+        proj4string(pt) <- proj4string(shp)
+        if (!is.na(sp::over(pt,shp)$ADMIN)) { # check if inside polygon
+          res <- rbind(res,data.frame(lat=y,long=x))
+        }
+      }
+    }
+  }
+  res
+}
+
+tzn_shp <- readOGR(dsn="./tzn_borders", layer="tzn_borders")
+
+sapply(1:nrow(zp),function(i) 
+  closest_dist(zp[i,'lat'],zp[i,'long'],tzn_data))
+
+zp <- get_zero_pts(ls,tzn_shp,200,tzn_data,cutoff=2)
+zp$zero <- TRUE
+tzn_data %>%
+  dplyr::select(lat,long) %>% 
+  mutate(zero=FALSE) %>%
+  rbind(zp) %>%
+  ggplot(aes(x=long,y=lat,color=zero)) +
+    geom_point(size=4)
+
+# TODO: Almost there! I just need a way to filter points and make sure 
+# they're not too close to sampled DHS points. And possibly relax the cutoff
+# a little.
+
+
+
+### old stuff below here
+
 library(curl)
 library(jsonlite)
 library(httr)
@@ -7,6 +93,7 @@ library(optimx)
 
 ###############################################################################
 # Get population density based on lat/long
+# Impossibly slow. Possible to do this with landscan instead?
 ###############################################################################
 options(stringsAsFactors = FALSE)
 get_pop_dens <- function(lat,long) {
@@ -28,19 +115,25 @@ get_pop_dens <- function(lat,long) {
 
 ###############################################################################
 # Get country based on lat/long
+# I'm getting rate-limited by the DSTK API; I need a better way to do this.
+# https://gis.stackexchange.com/questions/64513/checking-if-lng-and-lat-fall-inside-polygons-from-esri-shapefile
 ###############################################################################
-get_adm0 <- function(lat,long) {
-  url  <- "http://www.datasciencetoolkit.org"
-  path <- paste0('coordinates2politics/',lat,'%2c',long)
-  raw.result <- GET(url = url, path = path)
-  if (raw.result$status_code != 200) {
-    return(NULL)
+get_adm0 <- function(lat,long,method='google') {
+  if (method=='google') {
+    rgc <- revgeocode(location=c(long,lat),output='more')
+    return(as.character(rgc$country))
+  } else {
+    url  <- "http://www.datasciencetoolkit.org"
+    path <- paste0('coordinates2politics/',lat,'%2c',long)
+    raw.result <- GET(url = url, path = path)
+    if (raw.result$status_code != 200) {
+      return(NULL)
+    }
+    res <- raw.result$content %>% rawToChar %>% fromJSON
+    if (is.na(res$politics)) { return(NA) }
+    rp <- res$politics[[1]]
+    return(rp[rp$friendly_type=='country','name'])
   }
-  res <- raw.result$content %>% rawToChar %>% fromJSON
-  if (is.na(res$politics)) { return(NA) }
-  rp <- res$politics[[1]]
-  #paste0(lat,',',long,': ',rp[rp$friendly_type=='country','name']) %>% print
-  rp[rp$friendly_type=='country','name']
 }
 
 ###############################################################################
@@ -66,13 +159,9 @@ y.min <- min(df$lat)-y_spacing
 y.max <- max(df$lat)+y_spacing
 
 dummies <- data.frame()
-seed(12345)
-# add random noise to dummy points for better optimization start
 for (x in seq(x.min,x.max,x_spacing)) {
   for (y in seq(y.min,y.max,y_spacing)) {
-    rx <- runif(1,-dr,dr)
-    ry <- runif(1,-dr,dr)
-    new_dummy <- data.frame(lat=y+ry,long=x+rx,dummy=1)
+    new_dummy <- data.frame(lat=y,long=x,dummy=1)
     dummies <- rbind(dummies,new_dummy)
   }
 }
@@ -92,228 +181,31 @@ all <- rbind(df,dummies)
 ggplot(all,aes(x=long,y=lat,color=dummy)) +
   geom_point(size=4)
 
-###############################################################################
-# Optimize spacing between points
-#
-# I want to try doing this using optimx(), which requires an objective
-# function takes the input parameters as a vector and returns a scalar value.
-# Can use the gradient and Hessian as well if I'm feeling ambitious
-###############################################################################
-# Freeze all points that are not dummies or not in Tanzania
+# Query only dummies in Tanzania
 dummy_adm0 <- sapply(1:nrow(dummies),
-                     function(i) get_adm0(dummies$lat[i],dummies$long[i])) 
+                     function(i) {
+                       print(i)
+                       get_adm0(dummies$lat[i],dummies$long[i])}) 
 
-dummies$freeze <- sapply(dummy_adm0,
-                         function(x) !('United Republic of Tanzania' %in% x))
+dummies$query <- sapply(dummy_adm0,
+                         function(x) ('Tanzania' %in% x))
+
+
 all <- df %>%
-  mutate(freeze=TRUE) %>%
+  mutate(query=FALSE) %>%
   rbind(dummies)
-ggplot(all,aes(x=long,y=lat,color=freeze)) +
+
+all$adm0 <- NA
+all[all$dummy==0,'adm0'] <- 'Tanzania'
+all[all$dummy==1,'adm0'] <- sapply(dummy_adm0,function(x) 
+  ifelse('Tanzania' %in% x,'Tanzania',x[1]))
+
+ggplot(all,aes(x=long,y=lat,color=adm0)) +
   geom_point(size=4)
 
-melted <- c(all[!all$freeze,'lat'],all[!all$freeze,'long'])
-frozen <- c(all[all$freeze,'lat'],all[all$freeze,'long'])
-
-### Objective function: sum up the inverse of the distance to the nearest 
-### point (melted or frozen) for each melted point
-## For both sets of variables, assume that the first half of the vector is 
-## latitude values and the second half is longitude values
-
-# TODO: does it speed things up when I move this inside obj_fun()?
-closest_dist <- function(df,rows) {
-  sapply(rows,function(i) {
-    lati <- df[i,'lat']
-    longi <- df[i,'long']
-    df_i <- df[-i,] %>%
-      mutate(d2 = (lat-lati)^2 + (long-longi)^2) 
-    d2_min <- min(df_i$d2) %>% sqrt
-    d2_min
-  })
-}
-
-closest_wid <- function(df,rows) {
-  plyr::ldply(rows,function(i) {
-    lati <- df[i,'lat']
-    longi <- df[i,'long']
-    df_i <- df %>%
-      mutate(d2 = (lat-lati)^2 + (long-longi)^2) 
-    d2_min <- min(df_i$d2[df_i$d2 > 0]) 
-    closest <- which(df_i$d2 == d2_min) %>% head(1)
-    # The head(1) there is sort of arbitrary, but hopefully will only cause
-    # issues at the highly symmetric starting grid.
-    data.frame(r=sqrt(d2_min),id=closest)
-  })
-}
-
-obj_fun <- function(x,frozen=NULL) {
-  pts <- data.frame(lat=x[1:(length(x)/2)],
-                    long=x[(length(x)/2+1):length(x)],
-                    freeze=FALSE)
-  if (!is.null(frozen)) {
-    frzpts <- data.frame(lat=frozen[1:(length(frozen)/2)],
-                         long=frozen[(length(frozen)/2+1):length(frozen)],
-                         freeze=TRUE)
-    pts <- rbind(pts,frzpts)
-  }
-  val <- pts %>%
-    mutate(rn=row_number()) %>%
-    filter(!freeze) %>%
-    mutate(score=1/closest_dist(pts,rn)) %>%
-    select(score) %>%
-    sum
-  #print(val)
-  val
-}
-
-obj_fun(melted,frozen)
-
-x2df <- function(x,frozen=NULL) {
-  pts <- data.frame(lat=x[1:(length(x)/2)],
-                    long=x[(length(x)/2+1):length(x)],
-                    freeze=FALSE)
-  if (!is.null(frozen)) {
-    frzpts <- data.frame(lat=frozen[1:(length(frozen)/2)],
-                         long=frozen[(length(frozen)/2+1):length(frozen)],
-                         freeze=TRUE)
-    pts <- rbind(pts,frzpts)
-  }
-  pts %>% cbind(closest_wid(pts,1:nrow(pts)))
-}
-
-### Gradient function
-# For each melted point i, dV/dxi will have a contribution of
-# -(xi-xj)/(rij)^3 for the point j that is closest to i (melted or frozen)
-obj_fun.g <- function(x,frozen=NULL) {
-  neigh <- x2df(x,frozen)
-  lat_grad <- sapply(which(!neigh$freeze), function(i) {
-    j <- neigh[i,'id']
-    j_grad <- -(neigh[i,'lat']-neigh[j,'lat'])/neigh[i,'r']^3
-    k_list <- which(neigh$id == i)
-    if (length(k_list) > 0) {
-      k_grad <- sapply(k_list,function(k) {
-        -(neigh[i,'lat']-neigh[k,'lat'])/neigh[k,'r']^3
-      }) %>% sum
-    } else {k_grad <- 0}
-    j_grad + k_grad
-  }) 
-  long_grad <- sapply(which(!neigh$freeze), function(i) {
-    j <- neigh[i,'id']
-    j_grad <- -(neigh[i,'long']-neigh[j,'long'])/neigh[i,'r']^3
-    k_list <- which(neigh$id == i)
-    if (length(k_list) > 0) {
-      k_grad <- sapply(k_list,function(k) {
-        -(neigh[i,'long']-neigh[k,'long'])/neigh[k,'r']^3
-      }) %>% sum
-    } else {k_grad <- 0}
-    j_grad + k_grad
-  }) 
-  c(lat_grad,long_grad)
-}
-
-grad <- obj_fun.g(melted,frozen) 
-
-# TODO: Calculate Hessian. I suspect most off-diagonal elements will be zero
-
-#### Hessian function
-obj_fun.h <- function(x,frozen=NULL) {
-  neigh <- x2df(x,frozen)
-  n <- length(x)
-  m <- matrix(0,nrow=n,ncol=n)
-  for (i in 1:(n/2)) {
-    xi <- neigh[i,'long']
-    yi <- neigh[i,'lat']
-    j <- neigh[i,'id'] # j is i's nearest neighbor
-    xj <- neigh[j,'long']
-    yj <- neigh[j,'lat']
-    rij <- neigh[i,'r']
-    k_list <- which(neigh$id == i)
-    ## latitude first
-    # diagonal elements
-    # paste0('i = ',i) %>% print
-    # paste0('j = ',j) %>% print
-    # paste0('rij = ',rij) %>% print
-    # paste0('yi = ',yi) %>% print
-    # paste0('yi = ',yj) %>% print
-    mii <- -1/rij^3 + 3*(yi-yj)^2/rij^5
-    if (length(k_list) > 0) {
-      # neighbor contributions to diagonal
-      # TODO: do this in one loop instead of two
-      k_diag <- sapply(k_list,function(k) {
-        yk <- neigh[k,'lat']
-        rik <- neigh[k,'r']
-        -1/neigh[k,'r']^3 + 3*(yi-yk)^2/rik^5
-      }) %>% sum
-      mii <- mii + k_diag
-      # off-diagonal (latitude only)
-      for (k in k_list) {
-        yk <- neigh[k,'lat']
-        rik <- neigh[k,'r']
-        mik <- 1/rik^3 + 3*(yi-yk)^2/rik^5
-        if (neigh[i,'id']==k) { mik <- mik*2 }
-        m[i,k] <- mik
-      }
-    }
-    m[i,i] <- mii
-    ## now longitude
-    mii <- -1/rij^3 + 3*(xi-xj)^2/rij^5
-    if (length(k_list) > 0) {
-      # neighbor contributions to diagonal
-      k_diag <- sapply(k_list,function(k) {
-        xk <- neigh[k,'long']
-        rik <- neigh[k,'r']
-        -1/rik^3 + 3*(xi-xk)^2/rik^5
-      }) %>% sum
-      mii <- mii + k_diag
-      # off-diagonal (longitude only)
-      for (k in k_list) {
-        xk <- neigh[k,'long']
-        rik <- neigh[k,'r']
-        mik <- 1/rik^3 + 3*(xi-xk)^2/rik^5
-        if (neigh[i,'id']==k) mik <- mik*2
-        m[n/2+i,n/2+k] <- mik
-      }
-    }
-    m[n/2+i,n/2+i] <- mii
-    ## latitude-longitude cross terms
-    mii <- 3*(xi-xj)*(yi-yj)/rij^5
-    if (length(k_list) > 0) {
-      k_diag <- sapply(k_list,function(k) {
-        xk <- neigh[k,'long']
-        yk <- neigh[k,'lat']
-        rik <- neigh[k,'r']
-        3*(xi-xk)*(yi-yk)/rik^5
-      }) %>% sum
-      mii <- mii + k_diag
-      # off-diagonal cross terms
-      for (k in k_list) {
-        xk <- neigh[k,'long']
-        yk <- neigh[k,'lat']
-        rik <- neigh[k,'r']
-        mik <- 3*(xi-xk)*(yi-yk)/rik^5
-        if (neigh[i,'id']==k) mik <- mik*2
-        m[i,n/2+k] <- mik
-        m[n/2+i,k] <- mik
-      }
-    }
-    m[n/2+i,i] <- mii
-    m[i,n/2+i] <- mii
-  }
-  m
-}
-
-### Numeric test of Hessian
-n = 20
-melted <- runif(n)
-hess <- obj_fun.h(melted)
-m <- matrix(nrow=n,ncol=n)
-v <- obj_fun(melted)
-# get Hessian using numDeriv::hessian()
-# https://cran.r-project.org/web/packages/numDeriv/numDeriv.pdf
-
-opt <- optimx(melted,obj_fun,frozen=frozen)
-# This takes for-e-ver, because the default algorithm is Nelder-Mead simplex
-# if I want to use the more advanced L-BFGS-B, then I need to supply gradient
-# and Hessian.
+tzn <- all %>% filter(adm0=='Tanzania')
+tzn$pop_dens <- sapply(1:nrow(tzn),function(i)
+  get_pop_dens(dummies$lat[i],dummies$long[i]))
 
 # For set of sampling locations, find the point that is located within borders
 #   but has the largest possible radius around it with no points in it. (Rough heuristic OK.)
