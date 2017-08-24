09b7d2a3d8411f3f4dddebd9c8ca31bdd2570495
Craig Jolley <jolleycraig@gmail.com>
2017-08-24 12:03:07 -0400

    Forgot to commit this when I wrote it. I'm sure it's important.

--- low_density.R
+++ low_density.R
@@ -54,7 +54,8 @@ get_ls <- function(pts_df) {
 # Get the points with zero population density that are located within the 
 # country boundaries and are not too close to a DHS sample location.
 ###############################################################################
-get_zero_pts <- function(r,shp,npts,pt_df,cutoff=1,dist_cutoff=0.5,seed=12345) {
+get_zero_pts <- function(r,shp,npts,pt_df,cutoff=1,dist_cutoff=0.5,seed=12345,
+                         check_adm0=TRUE) {
   set.seed(seed)
   res <- data.frame()
   while (nrow(res) < npts) {
@@ -64,10 +65,16 @@ get_zero_pts <- function(r,shp,npts,pt_df,cutoff=1,dist_cutoff=0.5,seed=12345) {
     if (c > dist_cutoff) { # check if too close to sample points
       p <- extract(r,cbind(x,y))
       if (!is.na(p) && p < cutoff) { # check for zero population density
-        pt <- data.frame(x=x,y=y)
-        coordinates(pt) <- ~x+y
-        proj4string(pt) <- proj4string(shp)
-        if (!is.na(sp::over(pt,shp)$ADMIN)) { # check if inside polygon
+        addme <- TRUE
+        if (check_adm0) {
+          pt <- data.frame(x=x,y=y)
+          coordinates(pt) <- ~x+y
+          proj4string(pt) <- proj4string(shp)
+          if (is.na(sp::over(pt,shp)$ADMIN)) { # check if inside polygon
+            addme <- FALSE
+          }
+        } 
+        if (addme) {
           res <- rbind(res,data.frame(lat=y,long=x))
         }
       }
@@ -81,21 +88,24 @@ get_zero_pts <- function(r,shp,npts,pt_df,cutoff=1,dist_cutoff=0.5,seed=12345) {
 # borders, because EBK can't extrapolate beyond the sampled region. Find the
 # four points that make a bounding box around the entire country, and estimate
 # the target variable at those points using KNN.
+# TODO: Fix the code that's moving points; something isn't right here.
 ###############################################################################
 get_bounding_pts <- function(shp,pt_df,varname,buf_scale=0.025) {
-
+  shp_df <- shp2df(shp)
+  lat_max <- max(shp_df$lat)
+  lat_min <- min(shp_df$lat)
+  lat_buf <- buf_scale*(lat_max-lat_min)
+  long_max <- max(shp_df$long)
+  long_min <- min(shp_df$long)
+  long_buf <- buf_scale*(long_max-long_min)
   extrema <- shp_df[c(which.max(shp_df$lat),
                           which.max(shp_df$long),
                           which.min(shp_df$lat),
                           which.min(shp_df$long)),c('lat','long')] %>%
-    mutate(lat_=ifelse(lat==min(lat),
-                       min(lat)-buf_scale*(max(lat)-min(lat)),lat),
-           lat_=ifelse(lat==max(lat),
-                       max(lat)+buf_scale*(max(lat)-min(lat)),lat),
-           long_=ifelse(lat==min(long),
-                        min(long)-buf_scale*(max(long)-min(long)),long),
-           long_=ifelse(lat==max(long),
-                        max(long)+buf_scale*(max(long)-min(long)),long)) %>%
+    mutate(lat_=ifelse(lat==lat_min,lat_min-lat_buf,lat),
+           lat_=ifelse(lat_==lat_max,lat_max+lat_buf,lat_),
+           long_=ifelse(long==long_min,long_min-long_buf,long),
+           long_=ifelse(long_==long_max,long_max+long_buf,long_)) %>%
     select(lat_,long_) %>%
     rename(lat=lat_,long=long_)
   # Select optimal value of k for KNN model
@@ -150,13 +160,18 @@ extrema <- get_bounding_pts(tzn_shp,tzn_data,'mobile')
 shore <- get_shore_pts(ls,1000,tzn_data)
 
 ## Zero-point visualization
-tzn_data %>%
-  dplyr::select(lat,long) %>%
-  mutate(zero=FALSE) %>%
-  rbind(zp %>% mutate(zero=TRUE)) %>%
-  ggplot(aes(x=long,y=lat,color=zero)) +
+
+plot_zero <- function(df,zp) {
+  df %>%
+    dplyr::select(lat,long) %>%
+    mutate(zero=FALSE) %>%
+    rbind(zp %>% mutate(zero=TRUE)) %>%
+    ggplot(aes(x=long,y=lat,color=zero)) +
     geom_point(size=4) +
-    coord_equal()
+    coord_equal()  
+}
+plot_zero(tzn_data)
+  
 
 tzn_data %>%
   dplyr::select(lat,long,mobile) %>%
@@ -164,13 +179,15 @@ tzn_data %>%
   write.csv('tzn_zero.csv',row.names=FALSE)
 
 ## Extrema visualization
-tzn_data %>%
-  dplyr::select(lat,long) %>%
-  mutate(ex=FALSE) %>%
-  rbind(extrema %>% dplyr::select(lat,long) %>% mutate(ex=TRUE)) %>%
-  ggplot(aes(x=long,y=lat,color=ex)) +
-  geom_point(size=4) +
-  coord_equal()
+plot_ext <- function(df,extrema) {
+  df %>%
+    dplyr::select(lat,long) %>%
+    mutate(ex=FALSE) %>%
+    rbind(extrema %>% dplyr::select(lat,long) %>% mutate(ex=TRUE)) %>%
+    ggplot(aes(x=long,y=lat,color=ex)) +
+    geom_point(size=4) +
+    coord_equal()
+}
 
 tzn_data %>%
   dplyr::select(lat,long,mobile) %>%
@@ -179,17 +196,19 @@ tzn_data %>%
   write.csv('tzn_zero_ext.csv',row.names=FALSE)
 
 ## Shore visualization
-tzn_shore <- tzn_data %>%
-  dplyr::select(lat,long) %>% 
-  mutate(water=FALSE) %>%
-  rbind(shore %>% mutate(water=TRUE))
-
-tzn_shp %>% shp2df %>%
-  ggplot(aes(long,lat)) + 
-    geom_polygon(fill='gray85') +
-    geom_path(color="black") +
-    coord_equal() +
-    geom_point(data=tzn_shore,size=2,aes(color=water)) 
+plot_shore <- function(df,shp,shore) {
+  df_shore <- df %>%
+    dplyr::select(lat,long) %>% 
+    mutate(water=FALSE) %>%
+    rbind(shore %>% mutate(water=TRUE))
+  
+  shp %>% shp2df %>%
+    ggplot(aes(long,lat)) + 
+      geom_polygon(fill='gray85') +
+      geom_path(color="black") +
+      coord_equal() +
+      geom_point(data=df_shore,size=2,aes(color=water)) 
+}
 
 tzn_data %>%
   dplyr::select(lat,long,mobile) %>%
@@ -198,5 +217,33 @@ tzn_data %>%
   rbind(shore %>% mutate(mobile=0)) %>%
   write.csv('tzn_zero_ext_water.csv',row.names=FALSE)
 
-# TODO: Visualize the effects on the final kriged product (in AcrMap)
-# of adding in each of these components.
+###############################################################################
+# Uganda
+###############################################################################
+uga_data <- read.csv('uga_dhs_2011.csv') %>% filter(lat != 0)
+ls <- get_ls(uga_data)
+uga_shp <- readOGR(dsn="./uga_borders", layer="uga_borders")
+
+zp <- get_zero_pts(ls,uga_shp,200,uga_data,cutoff=1,check_adm0=FALSE) # use cutoff=1 for production
+plot_zero(uga_data,zp) # none of them inside Uganda
+extrema <- get_bounding_pts(uga_shp,uga_data,'mobile')
+extrema_elect <- get_bounding_pts(uga_shp,uga_data,'elect')
+plot_ext(uga_data,extrema)
+shore <- get_shore_pts(ls,50,uga_data)
+plot_shore(uga_data,uga_shp,shore)
+
+uga_data %>%
+  dplyr::select(lat,long,mobile) %>%
+  #rbind(zp %>% mutate(mobile=0)) %>%
+  rbind(extrema) %>%
+  rbind(shore %>% mutate(mobile=0)) %>%
+  write.csv('uga_zero_ext_water.csv',row.names=FALSE)
+
+# Downsample to fewer shore points for electricity, because I'm finding the
+# bounding region using SVM. SVM takes a lot longer to train and finds
+# much coarser surfaces.
+uga_data %>%
+  dplyr::select(lat,long,elect) %>%
+  rbind(extrema_elect) %>%
+  rbind(shore %>% mutate(elect=0) %>% head(50)) %>%
+  write.csv('uga_elect.csv',row.names=FALSE)
